07cf223998da3230facee9063a4500fd09582a24
HUOd <hrd2017ce@gmail.com>
2022-05-17 16:39:24 -0700

    outliers

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -9,8 +9,8 @@ from outlier_cleaner import outlierCleaner
 
 
 ### load up some practice data with outliers in it
-ages = joblib.load( open("./outliers/practice_outliers_ages.pkl", "rb") )
-net_worths = joblib.load( open("./outliers/practice_outliers_net_worths.pkl", "rb") )
+ages = joblib.load( open("practice_outliers_ages.pkl", "rb") )
+net_worths = joblib.load( open("practice_outliers_net_worths.pkl", "rb") )
 
 
 
@@ -26,14 +26,12 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
-
-
+print("slope:", reg.coef_)
+print("score:", reg.score(ages_test, net_worths_test))
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -67,6 +65,8 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print("new slope after cleaned:", reg.coef_)
+        print("new score after cleaned:", reg.score(ages_test, net_worths_test))
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print("You don't seem to have regression imported/created,")
