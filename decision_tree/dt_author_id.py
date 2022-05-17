e9400f8bbc0e3f496083dcb9fb0d0793f0bf4465
HUOd <hrd2017ce@gmail.com>
2022-05-16 23:13:44 -0700

    decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,19 +12,27 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print("Number of features: ", len(features_train[0]))
 
 
 #########################################################
 ### your code goes here ###
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+prediction = clf.predict(features_test)
+
+accuracy = accuracy_score(labels_test, prediction)
+print("Accuracy is:", accuracy)
 
 #########################################################
 
